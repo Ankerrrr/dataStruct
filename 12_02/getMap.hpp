f8be15951f06808e804b76b5600c189a2ea65f6f
Ankerrrr <80147818+Ankerrrr@users.noreply.github.com>
2024-12-02 23:17:27 +0800

    12_02

--- 12_02/getMap.hpp
+++ 12_02/getMap.hpp
@@ -4,39 +4,43 @@
 #include <iostream>
 #include <string>
 
-#define mazeRow 13 // �@
+//#define mazeRow 13 // 一
+//#define mazeCol 17 // 1
+
+#define mazeRow 17 // 一
 #define mazeCol 17 // 1
 
 using namespace std;
 
 int maze[mazeRow][mazeCol];
 
-bool loadMap(const string &filename) {
-  ifstream inputfile(filename);
-  if (!inputfile.is_open()) {
-    throw "cant not open File, plz check file name";
-  }
-
-  string line;
-  for (int i = 0; i < mazeRow; i++) {
-    if (!getline(inputfile, line) || line.size() != mazeCol) {
-      throw "problem occur read file";
-      inputfile.close();
-      return false;
+bool loadMap(const string& filename) {
+    ifstream inputfile(filename);
+    if (!inputfile.is_open()) {
+        throw "cant not open File, plz check file name";
     }
 
-    for (int j = 0; j < mazeCol; j++) {
-      if (line[j] >= '0' && line[j] < '2') {
-        maze[i][j] = line[j] - '0';
-      } else {
-        throw "problem occur read file";
-        inputfile.close();
-        return false;
-      }
+    string line;
+    for (int i = 0; i < mazeRow; i++) {
+        if (!getline(inputfile, line) || line.size() != mazeCol) {
+            throw "problem occur read file";
+            inputfile.close();
+            return false;
+        }
+
+        for (int j = 0; j < mazeCol; j++) {
+            if (line[j] >= '0' && line[j] < '2') {
+                maze[i][j] = line[j] - '0';
+            }
+            else {
+                throw "problem occur read file";
+                inputfile.close();
+                return false;
+            }
+        }
     }
-  }
-  inputfile.close();
-  return true;
+    inputfile.close();
+    return true;
 }
 
 #endif
